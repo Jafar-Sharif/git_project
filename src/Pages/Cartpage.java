4696c96208b9a1fa4ada1cbb2985ea073378e955
Jafar-Sharif <jafarsharif5349@gmail.com>
2022-07-13 12:35:44 +0530

    updated cart page

--- src/Pages/Cartpage.java
+++ src/Pages/Cartpage.java
@@ -4,6 +4,7 @@ public class Cartpage {
 
 	public static void main(String[] args) {
 		System.out.println("cart page");
+		System.out.println("cartpage feature");
 		// TODO Auto-generated method stub
 
 	}
